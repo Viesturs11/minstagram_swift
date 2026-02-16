1cb4b03e1cde8404c0b0933bf0b533fe960c07ed
Viesturs <viesturs.karlivans@gmail.com>
2026-02-16 15:50:54 +0200

    Initial Commit

---
+++ Minestagram_Karlivans/Minestagram_KarlivansApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minestagram_KarlivansApp.swift
+//  Minestagram_Karlivans
+//
+//  Created by Viesturs Karlivans on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Minestagram_KarlivansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
